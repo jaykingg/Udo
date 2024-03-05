78cb90b99f8b6ee63a82dd72fe9791594b0bc54d
jayking <mojunseo49@naver.com>
2024-03-05 13:00:53 +0900

    feat: IBSN annotation 추가

--- src/main/kotlin/com/company/udo/book/payload/BookCosignPayload.kt
+++ src/main/kotlin/com/company/udo/book/payload/BookCosignPayload.kt
@@ -2,12 +2,14 @@ package com.company.udo.book.payload
 
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.ISBN
 
 data class BookCosignPayload(
     @field: NotBlank(message = "도서명은 필수 값입니다.")
     val bookName: String,
 
     @field: NotBlank(message = "ISBN 코드는 필수 값입나다.")
+    @field: ISBN
     val isbn: String,
 
     @field: Min(0)
